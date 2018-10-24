0b24c3d5039deee349e18d8a6733bbe2d3554b16
ken-sayama <sayamaken0402@gmail.com>
2018-10-24 13:21:19 +0900

    change name

---
+++ sh/symlink.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# vim
+eval "ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc"
+eval "ln -sf ~/dotfiles/vim ~/.vim"
+
+# zsh
+eval "ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc"
+eval "ln -sf ~/dotfiles/zsh/.zshenv ~/.zshenv"
+
