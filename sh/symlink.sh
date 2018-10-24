c0bf3e0efeac61a55db68247277047821c9ecf87
ken-sayama <sayamaken0402@gmail.com>
2018-10-24 13:23:54 +0900

    bug fix

--- sh/symlink.sh
+++ sh/symlink.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # vim
-eval "ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc"
+eval "ln -sf ~/dotfiles/vim/vimrc ~/.vimrc"
 eval "ln -sf ~/dotfiles/vim ~/.vim"
 
 # zsh
-eval "ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc"
-eval "ln -sf ~/dotfiles/zsh/.zshenv ~/.zshenv"
+eval "ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc"
+eval "ln -sf ~/dotfiles/zsh/zshenv ~/.zshenv"
 
