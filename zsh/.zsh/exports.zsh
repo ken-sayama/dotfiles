804cfa3b992b65066040691c695b643c3bb65cbb
Ken Sayama <sayamaken0402@gmail.com>
2020-02-17 19:33:23 +0900

    create .zsh dir

---
+++ zsh/.zsh/exports.zsh
@@ -0,0 +1,37 @@
+# global
+export LANG=ja_JP.UTF-8
+export PATH="/usr/local/bin:$PATH"
+
+# zplug
+export ZPLUG_HOME=/usr/local/opt/zplug
+
+# xterm
+export XDG_CONFIG_HOME="$HOME/.config"
+export TERM=xterm-256color
+
+# Nodejs
+export PATH="/usr/local/share/npm/bin:$PATH"
+export PATH=$HOME/.nodebrew/current/bin:$PATH
+export PATH="$HOME/sls/node_modules/.bin/:$PATH"
+
+# rbenv
+export PATH="$HOME/.rbenv/bin:$PATH"
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# mysql
+export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+
+# golang
+export GOPATH=${HOME}/go
+export PATH=$GOPATH/bin:$PATH
+
+# PHP
+export PATH="$PATH:/Users/kensayama/.config/composer/vendor/bin"
+
+# FZF
+export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
+
+# Python
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
---
+++ zsh/.zsh/settings.sh
@@ -0,0 +1,3 @@
+setopt auto_cd
+setopt auto_list
+setopt auto_menu
